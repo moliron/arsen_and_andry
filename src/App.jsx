40fafca72704afc0de44ae6543f39f542b812d90
Cybersport <Cybersport@26kadr.internal>
2024-12-04 15:19:06 +0300

    V3

--- src/App.jsx
+++ src/App.jsx
@@ -22,10 +22,23 @@ import vector from './img/Vector.svg'
 import vector_2 from './img/Vector_1.svg'
 import vector_3 from './img/Vector_2.svg'
 import upp from './img/ArrowUpRight_1.svg'
+import model from './img/model1.svg'
+import model2 from './img/model2.svg'
+import model3 from './img/model3.svg'
+import model4 from './img/model4.svg'
+import but from './img/Button.svg'
+import but2 from './img/Button2.svg'
+import frame1 from './img/Group 2.svg'
+import frame4 from './img/Frame4.svg'
+import frame5 from './img/Frame5.svg'
+import frame6 from './img/Frame6.svg'
+import frame7 from './img/Frame7.svg'
+import frame8 from './img/Frame8.svg'
+import home from './img/Home.svg'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App color">
       <header className='box items-center flex justify-between px-4 py-3'>
         <img src={dots} alt="" />
         <p className='font-bold text-[17px]'>SUMMER SALE UP TO 50% OFF</p>
@@ -67,7 +80,7 @@ function App() {
         <p className='font-bold ml-[-50px] mt-[-170px]'>€49</p>
       </div>
       <div className="">
-        <div className="bg-white w-full h-[44px] "></div>
+        <div className=" w-full h-[44px] "></div>
         <div className="men w-[596px] h-[636px] ml-80 rounded-[10px] relative">
           <img src={men} alt="" />
           <button className='bg-black text-white absolute ml-[20px] px-4 py-3 bottom-10 rounded-[10px] '>Shop Sweaters</button>
@@ -90,30 +103,109 @@ function App() {
         <img src={up} alt="" className='px-[5px]' />
       </div>
       <div className="">
-        <div className="bg-white w-full h-[54px] "></div>
-      <div className="text-[40px] flex justify-center font-bold">
-        <p>Hot off the press</p>
+        <div className=" w-full h-[54px] "></div>
+        <div className="text-[40px] flex justify-center font-bold">
+          <p>Hot off the press</p>
+        </div>
+        <div className="ml-[500px] w-[900px] h-[270px] flex border-[2px] rounded-[10px] solid black;">
+          <div className='border-r-2 ml-11 mt-11 mb-[10px] '>
+            <img src={vector} alt="" />
+            <p className="mt-6 w-[250px] h-[200px]">“First Light: Where great design and the finest materials unite in ultimate comfort”</p>
+            <p className='mt-[-80px] text2'>Read article</p>
+            <img src={upp} alt="" className='flex mt-[-20px] ml-[80px]' />
+          </div>
+          <div className="ml-11 mt-11 mb-[10px] border-r-2">
+            <img src={vector_2} alt="" />
+            <p className='mt-6 w-[250px] h-[200px]'>“First light proves it is possible to combine great design with the finest materials available.”</p>
+            <p className='mt-[-70px] text2'>Read article</p>
+            <img src={upp} alt="" className='flex mt-[-20px] ml-[80px]' />
+          </div>
+          <div className="ml-11 mt-11">
+            <img src={vector_3} alt="" />
+            <p className='mt-6 w-[250px] h-[200px]'>“Redefining excellence with premium materials and visionary robust design.”</p>
+            <p className='mt-[-70px] text2'>Read article</p>
+            <img src={upp} alt="" className='flex mt-[-20px] ml-[80px]' />
+          </div>
+        </div>
       </div>
-      <div className="ml-[450px] w-[1000px] h-[260px] flex border-[2px] solid black;">
-        <div className='border-r-2 ml-8 mt-11 mb-[10px] '>
-          <img src={vector} alt="" />
-          <p  className="mt-6 w-[250px] h-[200px]">“First Light: Where great design and the finest materials unite in ultimate comfort”</p>
-          <p className='mt-[-80px] text2'>Read article</p>
-          <img src={upp} alt="" className='flex mt-[-20px] ml-[80px]' />
-        </div>
-        <div className="ml-20 mt-11 mb-[10px] border-r-2">
-          <img src={vector_2} alt="" />
-          <p className='mt-6 w-[250px] h-[200px]'>“First light proves it is possible to combine great design with the finest materials available.”</p>
-          <p className='mt-[-70px] text2'>Read article</p>
-          <img src={upp} alt="" className='flex mt-[-20px] ml-[80px]' />
-        </div>
-        <div className="ml-20 mt-11">
-          <img src={vector_3} alt="" />
-          <p className='mt-6 w-[250px] h-[200px]'>“Redefining excellence with premium materials and visionary robust design.”</p>
-          <p className='mt-[-70px] text2'>Read article</p>
-          <img src={upp} alt="" className='flex mt-[-20px] ml-[80px]' />
+      <div className="">
+        <p className='mt-9 text-[40px] font-bold items-center justify-center flex'>Women — Must Haves</p>
+        <div className="mt-11 items-center flex justify-center gap-8">
+          <img src={but} alt="" />
+          <div className="">
+            <div className="men w-[320px] h-[420px] rounded-[10px] "><img src={model} alt="" /></div>
+            <div className="flex justify-between mt-6">
+              <p>Basic Shirt</p>
+              <p>€49</p>
+            </div>
+          </div>
+          <div className="">
+            <div className="men w-[320px] h-[420px] rounded-[10px] "><img src={model2} alt="" /></div>
+            <div className="flex justify-between mt-6">
+              <p>Beige Jumper</p>
+              <p>€99</p>
+            </div>
+          </div>
+          <div className="">
+            <div className="men w-[320px] h-[420px] rounded-[10px] "><img src={model3} alt="" /></div>
+            <div className="flex justify-between mt-6">
+              <p>Signature Jacket</p>
+              <p>€249</p>
+            </div>
+          </div>
+          <div className="">
+            <div className="men w-[320px] h-[420px] rounded-[10px] "><img src={model4} alt="" /></div>
+            <div className="flex justify-between mt-6">
+              <p>Navy Jumper</p>
+              <p>€49</p>
+            </div>
+          </div>
+          <img src={but2} alt="" />
         </div>
       </div>
+      <div className="relative">
+        <p className='flex justify-center items-center mt-6 text-[40px]'>We always have your back</p>
+        <img src={frame1} alt="" className='mt-[20px] ml-[340px]' />
+        <button className='bg-black text-white absolute px-4 py-2 left-10 bottom-3 ml-[450px] rounded-[10px]'>100% Premium Cotton <br /> We use ethically sourced cotton only.</button>
+        <button className='bg-black text-white absolute px-4 py-2 left-10 bottom-[343px] ml-[1100px] rounded-[10px]'>Made in Portugal <br />With hand finished details </button>
+        <button className='bg-black text-white absolute px-4 py-2 left-10 bottom-3 ml-[1070px] rounded-[10px]'>Breathable material<br />Keeping you cool throughout the day</button>
+      </div>
+      <div className="flex justify-between border-b-2 mt-10 ml-[340px] border-t-2 w-[1216px] h-16 ">
+        <div className="flex">
+          <img src={frame4} alt="" />
+          <p className='mt-2 ml-4'>Premium stain <br /> resistant fabrics</p>
+        </div>
+        <div className="flex">
+          <img src={frame5} alt="" />
+          <p className='mt-2 ml-4'>Free Express  <br /> Shipping</p>
+        </div>
+        <div className="flex">
+          <img src={frame6} alt="" />
+          <p className='mt-2 ml-4'>Free returns  <br /> within 30 days</p>
+        </div>
+        <div className="flex">
+          <img src={frame7} alt="" />
+          <p className='mt-2 ml-4'>Customers rate<br />  us 4.8 / 5.0</p>
+        </div>
+      </div>
+      <div className="flex items-center">
+        <img src={frame8} alt="" className='mt-10 ml-[340px] rounded-l-md' />
+        <div className="-mt-10">
+          <div className="bg-white h-[610px] w-[608px]  rounded-r-md mt-10">
+            <div className="ml-12">
+            <p className='text-[32px] py-16 mt-20'>Visit our popup shop</p>
+            <p className='-mt-3'>Willemsparkweg 63 <br/> 1071 GS Amsterdam <br/> The Netherlands</p>
+            <div className="mt-7 flex">
+            <div className="">
+            <p className='h-[100px] w-[102px] leading-10 text-[16px]'>Monday Tuesday Wednesday Thursday Friday Saturday Sunday</p>
+            </div>
+            <div className="">
+            <p className='h-[10px] w-[100px] ml-28 leading-10 text-[16px]'>12.00 - 19.00 10.00 - 19.00 10.00 - 19.00 10.00 - 21.00 10.00 - 19.00 10.00 - 18.00 12.00 - 18.00</p>
+            </div>
+            </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
