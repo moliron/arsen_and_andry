4a722166cc75b6cab2dcc8b6a97c4e38ab14e0ff
Cybersport <Cybersport@26kadr.internal>
2024-12-05 14:06:02 +0300

    V4

--- src/App.jsx
+++ src/App.jsx
@@ -135,28 +135,28 @@ function App() {
         <div className="mt-11 items-center flex justify-center gap-8">
           <img src={but} alt="" className='scalee' />
           <div className="">
-            <div className="men w-[320px] h-[420px] scale blur-image rounded-[10px] "><img src={model} alt="" /></div>
+            <div className="men w-[320px] h-[420px] scale rounded-[10px] "><img src={model} alt="" /></div>
             <div className="flex justify-between mt-6">
               <p>Basic Shirt</p>
               <p>€49</p>
             </div>
           </div>
           <div className="">
-            <div className="men w-[320px] h-[420px] scale blur-image rounded-[10px] "><img src={model2} alt="" /></div>
+            <div className="men w-[320px] h-[420px] scale rounded-[10px] "><img src={model2} alt="" /></div>
             <div className="flex justify-between mt-6 ">
               <p>Beige Jumper</p>
               <p>€99</p>
             </div>
           </div>
           <div className="">
-            <div className="men w-[320px] h-[420px] scale blur-image rounded-[10px] "><img src={model3} alt="" /></div>
+            <div className="men w-[320px] h-[420px] scale rounded-[10px] "><img src={model3} alt="" /></div>
             <div className="flex justify-between mt-6">
               <p>Signature Jacket</p>
               <p>€249</p>
             </div>
           </div>
           <div className="">
-            <div className="men w-[320px] h-[420px] scale blur-image rounded-[10px] "><img src={model4} alt="" /></div>
+            <div className="men w-[320px] h-[420px] scale rounded-[10px] "><img src={model4} alt="" /></div>
             <div className="flex justify-between mt-6">
               <p>Navy Jumper</p>
               <p>€49</p>
